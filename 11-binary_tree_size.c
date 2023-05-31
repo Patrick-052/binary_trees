0f1f8d69ad6be1e585669e32fd46da763d83f382
Patrick-052 <muiruripatrick052@gmail.com>
2023-05-31 17:18:34 +0300

    size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,9 +10,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	size_t ls = 0, rs = 0;
-
-	ls = binary_tree_size(tree->left);
-	rs = binary_tree_size(tree->right);
-	return (ls + rs + 1);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
