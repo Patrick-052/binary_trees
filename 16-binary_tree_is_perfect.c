0473533ea9804e47dd325217dc8eaffb3468cccf
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-01 14:19:26 +0300

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -63,7 +63,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (node_d == level++);
+		return (node_d == level);
 
 	if (binary_tree_is_perfect(tree->left) &&
 		binary_tree_is_perfect(tree->right))
